cee64c0450b8b66f3ef6d8298a008f5b3ad31bb6
Julutus <julian94info@gmail.com>
2024-09-06 08:38:54 +0000

    Third Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const subject = [
+  "My dog",
+  "The weather",
+  "My alarm",
+  "The 4Geeks Academy",
+  "Bob Sinclair"
+];
+const verb = ["ate", "broke", "lost", "hid", "destroyed"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const object = [
+  "my homework",
+  "the keys",
+  "my phone",
+  "the report",
+  "the presentation"
+];
+
+const time = [
+  "yesterday",
+  "this morning",
+  "last night",
+  "a few minutes ago",
+  "right before the meeting"
+];
+
+function generateExcuse() {
+  const randomSubject = Math.floor(Math.random() * subject.length);
+  const randomVerb = Math.floor(Math.random() * verb.length);
+  const randomObject = Math.floor(Math.random() * object.length);
+  const randomTime = Math.floor(Math.random() * time.length);
+
+  return (
+    subject[randomSubject] +
+    ` ` +
+    verb[randomVerb] +
+    ` ` +
+    object[randomObject] +
+    ` ` +
+    time[randomTime]
+  );
+}
+
+function displayExcuse() {
+  document.getElementById("excuse").textContent = generateExcuse();
+}
+
+function generateAndReload() {
+  displayExcuse();
+  window.location.reload();
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  displayExcuse();
 };
